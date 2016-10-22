8b0c6871c18ae4bb5c0703ab510b107fcc8ddb07
prkandel <prkandel19@gmail.com>
2016-10-23 01:21:33 +0545

    Add data-row, data-column and id attribute for each cell

--- js/golWindow.js
+++ js/golWindow.js
@@ -1,10 +1,9 @@
 var createGrid = function(container, grid){
 	var rows = grid.getCells().length;
 	var columns = grid.getCells()[0].length;
-	console.log(columns);
 	for(var i=0; i<rows; i++){
 		for(var j=0; j<columns; j++){
-			var $cell = $( "<div class='cell'></div>" );
+			var $cell = $( "<div class='cell'></div>" ).attr("id", "row"+i+"column"+j).attr("data-row",i).attr("data-column",j);
 			$cell.css( { left: j*$(".cell").width(), top: i*$(".cell").height() } );
 			container.append($cell);
 		}
