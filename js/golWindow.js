f61347c24a0d92bcad0ef5fffcf7454a76db3a8e
prkandel <prkandel19@gmail.com>
2016-10-23 02:36:54 +0545

    Add onclick event for all cells

--- js/golWindow.js
+++ js/golWindow.js
@@ -3,9 +3,24 @@ var createGrid = function(container, grid){
 	var columns = grid.getCells()[0].length;
 	for(var i=0; i<rows; i++){
 		for(var j=0; j<columns; j++){
-			var $cell = $( "<div class='cell'></div>" ).attr("id", "row"+i+"column"+j).attr("data-row",i).attr("data-column",j);
+			var divid = "row"+i+"column"+j;
+			var $cell = $( "<div class='cell'></div>" ).attr("id", divid).attr("data-row",i).attr("data-column",j);
 			$cell.css( { left: j*$(".cell").width(), top: i*$(".cell").height() } );
+			$cell.click(updateCell(grid, i, j));
 			container.append($cell);
+		}
+	}
+}
+
+function updateCell(grid,row, column){
+	return function(){
+		var divid = "#row"+row+"column"+column;
+		if(grid.getCells()[row][column] == "dead"){
+			grid.getCells()[row][column] = "alive";
+			$(divid).addClass("alive");
+		}else{
+			grid.getCells()[row][column] = "dead";
+			$(divid).removeClass("alive");
 		}
 	}
 }
